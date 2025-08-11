5375446f980370faff7c118efc0c602afd4b7373
pablo <aguilarchinomepablonicolas@gmail.com>
2025-08-11 17:02:31 -0500

    envio de datos festivos
    se agrega informacion de festivos de mongui

---
+++ src/components/pages/Festivos.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+const Festivos = () => {
+  return (
+    <div>
+      <h1>Días Festivos en Boyacá</h1>
+      <ul>
+        <li>Viernes 3, 4 y 5 de enero de 2025</li>
+        <li>FESTIVIDAD EN HONOR A LA SANTISIMA VIRGEN DEL CARMEN PATRONA DE LOS TRANSPORTADORES DE LA VILLA DE MONGUI EN EL PORTÓN BLANCO LOS DIAS 30 Y 31 DE JULIO DEL 2022.</li>
+        <li>20 de marzo - Día de San José</li>
+        <li>14 de abril - Jueves Santo</li>
+        <li>15 de abril - Viernes Santo</li>
+        <li>1 de mayo - Día del Trabajo</li>
+        <li>29 de junio - San Pedro y San Pablo</li>
+        <li>20 de julio - Independencia</li>
+        <li>7 de agosto - Batalla de Boyacá</li>
+        <li>15 de agosto - Asunción de la Virgen</li>
+        <li>12 de octubre - Día de la Raza</li>
+        <li>1 de noviembre - Todos los Santos</li>
+        <li>11 de noviembre - Independencia de Cartagena</li>
+        <li>8 de diciembre - Inmaculada Concepción</li>
+        <li>25 de diciembre - Navidad</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Festivos;
--- src/data/monguiData.ts
+++ src/data/monguiData.ts
@@ -1,3 +1,11 @@
+export interface Festivo {
+  id: number,
+  title: string,
+  date: string,
+  description: string
+}
+
+
 export interface Attraction {
   id: number;
   name: string;
@@ -25,6 +33,62 @@ export interface TravelTip {
   description: string;
   category: 'accommodation' | 'transportation' | 'food' | 'weather' | 'general';
 }
+
+export const Festivos : Festivo[]=[
+  {
+    id: 1,
+    title: 'Día de Reyes',
+    date: '3, 4 y 5 de enero',
+    description: 'Las celebraciones en Monguí comienzan en enero con el Día de Reyes, una antigua tradición que ya cumple 200 años. Todos los años se celebra con dramas organizados por la parroquia.'
+  },
+  {
+    id: 2,
+    title: 'Semana Santa',
+    date: 'marzo/abril',
+    description: 'Gracias al interés de un pequeño colectivo fue recuperada esta fiesta que tiene todo el sentido de la fe y de la cultura católica.'
+  },
+  {
+    id: 3,
+    title: 'Celebración de la Virgen',
+    date: 'Mayo',
+    description: ' Durante el mes de mayo es la celebración de la Virgen. Todos los días por sus calles se ve la procesión acompañada de música, poesía y oraciones. Participan niños, jóvenes, adultos y abuelos. Familias de las zonas urbana y rural se congregan en la festividad.'
+  },{
+    id: 4,
+    title: 'Corpus Christi',
+    date: 'Junio',
+    description: 'Es la manifestación de adoración a Dios en la eucaristía. La expresión artística también sirve para alagarlo.'
+  },{
+    id: 5,
+    title: 'Sagrado Corazón',
+    date: 'Junio',
+    description: 'Junio es el mes de esta fiesta en la que se da gracias por la paz del pueblo. Expresiones culturales y políticas se toman sus calles y sitios de encuentro.'
+  },{
+    id: 6,
+    title: 'San Antonio',
+    date: 'Junio',
+    description: 'Esta fiesta es toda una tradición en la población. Cuenta con una gran actividad cultural, procesiones y misas.'
+  },{
+    id: 7,
+    title: 'La fiesta del Tránsito.',
+    date: 'agosto',
+    description: 'Es una fiesta importante, más de fe que de cultura y turismo. Tiene novena, eucaristías, procesión y en cada fiesta se quema pólvora.'
+  },{
+    id: 8,
+    title: 'Fiesta de la Virgen del Carmen',
+    date: '30 y 31 de julio',
+    description: 'Festividad en honor a la Santísima Virgen del Carmen, patrona de los transportadores de la Villa de Monguí en el Portón Blanco.'
+  },{
+    id: 9,
+    title: 'Fiesta de San Pedro y San Pablo',
+    date: 'junio',
+    description: 'Celebración religiosa que incluye procesiones y eventos culturales.'
+  },{
+    id: 10,
+    title: 'Feria del Balón',
+    date: 'agosto',
+    description: 'Este festival resalta los saberes y la creatividad existente en este destino turístico, esto destaca que la fabricación de balones cocidos a mano de manera artesanal, es una tradición de más de 80 años y para conmemorar estos saberes se han preparado actividades como la feria comercial y artesanal, talleres de elaboración de balones, concursos, presentaciones artísticas y culturales.'
+  }
+]
 
 export const attractions: Attraction[] = [
   {
